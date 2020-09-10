07c56fd164f288129d4bb5ea30c10ebba26522c6
zmardil <btzardil@gmail.com>
2020-09-11 04:41:19 +0530

    foo change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "./style.css";
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>@zmardil</h1>
+      <p>Web Designer/Developer</p>
     </div>
   );
 }
